b899538cf64b366838b6942ad33d8188c14a8fb0
John-Morgan Manos <jmanos@sermeq.ess.washington.edu>
2021-11-01 10:47:20 -0400

    actually all the stuff

--- compile_fit.py
+++ compile_fit.py
@@ -6,11 +6,11 @@ MAX_EPOCHS = 20
 
 def compile_and_fit(model, window, patience=2):
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
-                                                    patience=patience,
+                                                    patience=5,
                                                     mode='min')
 
     model.compile(loss=tf.losses.MeanSquaredError(),
-                optimizer=tf.optimizers.Adam(),
+                optimizer=tf.optimizers.Adam(learning_rate=0.001),
                 metrics=[tf.metrics.MeanAbsoluteError()])
 
     history = model.fit(window.train, epochs=MAX_EPOCHS,
--- cross_val_training.py
+++ cross_val_training.py
@@ -9,17 +9,19 @@ import seaborn as sns
 from d2d import *
 from compile_fit import *
 from model_creator import *
+from datetime import datetime
 
-def plot_loss(history, model = 'linear'):
-    plt.plot(history.history['loss'], label='loss')
-    plt.plot(history.history['val_loss'], label='val_loss')
-    plt.ylim([0, 1])
-    plt.xlabel('Epoch')
-    plt.ylabel('Error')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig("loss_curves/" + model + "_loss_curve.png", dpi=300, bbox_inches='tight')
-    plt.clf()
+# datetime object containing current date and time
+
+now = datetime.now()
+dt_string = now.strftime("%d_%m_%Y_%H_%M")
+
+#Plotting
+p = 1
+
+fig = plt.figure(figsize=(6, 14), facecolor='w', edgecolor='k')
+
+#chunking and defining the data
 
 n = 10000  #chunk row size
 list_df = [df_all_chan[i:i+n] for i in range(0,df_all_chan.shape[0],n)]
@@ -28,7 +30,11 @@ val_performance={}
 performance={}
 history = {}
 
+#cross validation training
+
 k = 1
+a = 1
+
 
 for i in list_df:
     n = len(i)
@@ -56,38 +62,75 @@ for i in list_df:
         label_columns=['Discharge'],
         input_columns=input_columns)
     
+    #Linear model
+    
     history['linear_fold'+str(k)] = compile_and_fit(linear, multi_step_window)
 
     val_performance['Multistep_Linear_fold' + str(k)] = linear.evaluate(multi_step_window.val)
     performance['Multistep_Linear_fold' + str(k)] = linear.evaluate(multi_step_window.test, verbose=0)
-    
-    plot_loss(history['linear_fold'+str(k)], model = 'linear_fold'+str(k))
+
+    plt.subplot(7, 3, a)
+    plt.plot(history['linear_fold'+str(k)].history['loss'], label='loss')
+    plt.plot(history['linear_fold'+str(k)].history['val_loss'], label='val_loss')
+    plt.ylim([0, 1])
+    plt.xlabel('Epoch')
+    plt.ylabel('Error')
+    plt.legend()
+    plt.grid(True)
+    plt.title("Loss Curve Linear, fold: "+str(k))
+    a += 1
+    #DNN model
     
     history['dnn_fold'+str(k)] = compile_and_fit(dnn_model, multi_step_window)
 
     val_performance['Multistep_DNN_fold' + str(k)] = dnn_model.evaluate(multi_step_window.val)
     performance['Multistep_DNN_fold' + str(k)] = dnn_model.evaluate(multi_step_window.test, verbose=0)
     
-    plot_loss(history['dnn_fold'+str(k)], model = 'dnn_fold' + str(k))
+    plt.subplot(7, 3, a)
+    plt.plot(history['dnn_fold'+str(k)].history['loss'], label='loss')
+    plt.plot(history['dnn_fold'+str(k)].history['val_loss'], label='val_loss')
+    plt.ylim([0, 1])
+    plt.xlabel('Epoch')
+    plt.ylabel('Error')
+    plt.legend()
+    plt.grid(True)
+    plt.title("Loss Curve DNN, fold: "+str(k))
+    a += 1
+                
+    #LSTM model
     
     history['lstm_fold'+str(k)] = compile_and_fit(lstm_model, multi_step_window)
 
     val_performance['Multistep_LSTM_fold' + str(k)] = lstm_model.evaluate(multi_step_window.val)
     performance['Multistep_LSTM_fold' + str(k)] = lstm_model.evaluate(multi_step_window.test, verbose=0)
     
-    plot_loss(history['lstm_fold'+str(k)], model = 'lstm_fold' + str(k))
+    plt.subplot(7, 3, a)
+    plt.plot(history['lstm_fold'+str(k)].history['loss'], label='loss')
+    plt.plot(history['lstm_fold'+str(k)].history['val_loss'], label='val_loss')
+    plt.ylim([0, 1])
+    plt.xlabel('Epoch')
+    plt.ylabel('Error')
+    plt.legend()
+    plt.grid(True)
+    plt.title("Loss Curve DNN, fold: "+str(k))
+    a += 1
     
     print('Done with fold: ' + str(k))
     
     k += 1
     
-    
+#saving the loss curves into a figure
 
+plt.savefig("loss_curves/" + "loss_curves"+dt_string+".png", dpi=300, bbox_inches='tight')
+
+#saving the performance metrics
 
 file = open('performance_metrics.txt', 'a')
 file.write('performance: ' + str(performance) + '    ')
 file.write('val_performance: ' + str(val_performance))
 file.close()
+
+#saving the models
 
 linear.save('saved_models/linear_model_h5.h5')
 dnn_model.save('saved_models/dnn_model_h5.h5')
